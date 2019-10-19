e5ace19eaac92793cb2f9bc96ee5a0b34cbe46c0
divya chandran <divyachandran.ds@gmail.com>
2019-10-19 11:25:08 -0400

    add changes

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 
 from app import app
-from apps import main5, code
+from apps import main, code
 
 if 'DYNO' in os.environ:
     app_name = os.environ['DASH_APP_NAME']
@@ -21,7 +21,7 @@ app.layout = html.Div([
               [Input('url', 'pathname')])
 def display_page(pathname):
     if pathname is None or pathname.replace(app_name, '').strip('/') == '':
-        return main5.layout
+        return main.layout
     else:
         return code.layout
 
